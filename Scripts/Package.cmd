aedc732d6c1a0fa8d2b7c95a412a20de4fb0987d
Martin Dubois <mdubois@kms-quebec.com>
2018-10-26 17:17:54 -0400

    Export and package the Radar sample

--- Scripts/Package.cmd
+++ Scripts/Package.cmd
@@ -13,27 +13,48 @@ set KMS_ZIP_EXE="C:\Software\KmsTools\KmsZip.exe"
 
 rem ===== Verification ======================================================
 
-if not exist %KMS_ZIP_EXE% (
-	echo FATAL ERROR  %KMS_ZIP_EXE%  does not exist
-	echo Install KmsTools 2.4.0 or higher
+if ""=="%1" (
+    echo USER ERROR  Invalid command line
+	echo Usage  Packet.cmd {Version_Type}
 	pause
 	exit /B 1
 )
 
+if not exist %KMS_ZIP_EXE% (
+	echo FATAL ERROR  %KMS_ZIP_EXE%  does not exist
+	echo Install KmsTools 2.4.0 or higher
+	pause
+	exit /B 2
+)
+
 rem ===== Execution =========================================================
+
+%KMS_ZIP_EXE% OpenNet_Samples_%1.zip Package_Samples.txt .
+if ERRORLEVEL 1 (
+	echo ERROR  %KMS_ZIP_EXE% OpenNet_Samples_%1.zip Package_Samples.txt .  failed - %ERRORLEVEL%
+	pause
+	exit /B 3
+)
 
 %KMS_ZIP_EXE% OpenNet_SDK_%1.zip Package_SDK.txt .
 if ERRORLEVEL 1 (
 	echo ERROR  %KMS_ZIP_EXE% OpenNet_SDK_%1.zip Package_SDK.txt .  failed - %ERRORLEVEL%
 	pause
-	exit /B 2
+	exit /B 4
 )
 
 %KMS_ZIP_EXE% OpenNet_SDK_DDK_%1.zip Package_SDK_DDK.txt .
 if ERRORLEVEL 1 (
 	echo ERROR  %KMS_ZIP_EXE% OpenNet_SDK_DDK_%1.zip Package_SDK_DDK.txt .  failed - %ERRORLEVEL%
 	pause
-	exit /B 3
+	exit /B 5
+)
+
+%KMS_ZIP_EXE% OpenNet_SDK_Headers_%1.zip Package_SDK_Headers.txt .
+if ERRORLEVEL 1 (
+	echo ERROR  %KMS_ZIP_EXE% OpenNet_SDK_Headers_%1.zip Package_SDK_Headers.txt .  failed - %ERRORLEVEL%
+	pause
+	exit /B 6
 )
 
 rem ===== End ===============================================================
